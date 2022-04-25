605ce45eb7f5627b5d010402b06e299a45d4de68
KaylaJo <91999948+KaylaJo@users.noreply.github.com>
2022-04-25 16:23:11 -0500

    Set up first movies rows with api request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from "./logo.svg";
 import React from "react";
 import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
 //https://api.themoviedb.org/3/movie/550?api_key=4bb02057b03105e016159ce427638d9e
 //api_key =4bb02057b03105e016159ce427638d9e
 
 function App() {
-  return <div className="App">testing</div>;
+  return (
+    <div className="App">
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflix} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+    </div>
+  );
 }
 
 export default App;
