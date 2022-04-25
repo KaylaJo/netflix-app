5593cf020ce24c31f3fb714dff736fd7ce1fe9d3
KaylaJo <91999948+KaylaJo@users.noreply.github.com>
2022-04-25 14:39:04 -0500

    Initial Set Up, installed Axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+//https://api.themoviedb.org/3/movie/550?api_key=4bb02057b03105e016159ce427638d9e
+//api_key =4bb02057b03105e016159ce427638d9e
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">test</div>;
 }
 
 export default App;
