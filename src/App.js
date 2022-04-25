e983a36557f631a16aafb4d7043569a1d5fd49ea
KaylaJo <91999948+KaylaJo@users.noreply.github.com>
2022-04-25 17:41:56 -0500

    Added Poster Images for Movies

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
-//https://api.themoviedb.org/3/movie/550?api_key=4bb02057b03105e016159ce427638d9e
-//api_key =4bb02057b03105e016159ce427638d9e
 
 function App() {
   return (
--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,8 @@
+import movieTrailer from "movie-trailer";
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
+
+const base_url = "https://image.tmdb.org/t/p/original";
 
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
@@ -15,8 +18,19 @@ function Row({ title, fetchUrl }) {
   console.log(movies);
 
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
+      <div className="row__posters">
+        {}
+        {movies.map((movie) => (
+          <img
+            key={movie.toString()}
+            className="Row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.title}
+          />
+        ))}
+      </div>
     </div>
   );
 }
